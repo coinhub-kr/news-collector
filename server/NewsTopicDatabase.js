e0cb8dfcf020afd655919a8065b3994fb7b52ff4
an <adh0463@gmail.com>
2022-05-17 23:00:51 +0900

    Update: remove useless codes

--- server/NewsTopicDatabase.js
+++ server/NewsTopicDatabase.js
@@ -5,10 +5,10 @@ mongoose.connect('mongodb://localhost:27017/test');
 var db = mongoose.connection;
 
 db.on('error', function(){
-    console.log('Connection Failed!');
+    Logger.error(`Fail to connect to database.`);
 });
 db.once('open', function() {
-    console.log('Connected!');
+    Logger.info(`Connected to database.`);
 });
 /**
  * MongoDB
@@ -18,38 +18,9 @@ var newsSchema = mongoose.Schema({
     summary : 'string',
     date : 'date'
 });
-// var Student2 = mongoose.model('Schema', student2);
-// var newStudent = new Student2({name:'Hong Gil Dong', address:'서울시 강남구 논현동', age:'22'});
 
-var NewsModel = mongoose.model('news', newsSchema);
-
-var testData = {
-    title: "Test title",
-    summary: "test summary",
-    date: new Date('2014-01-22T14:56:59.301Z')
-};
-// var newNews = new NewsModel(testData);
-// newNews.save(function(error, data){
-//     if(error){
-//         console.log(error);
-//     } else {
-//         console.log('save');
-//     }
-// });
 var newsCollection = db.collection('news');
-// newsCollection.findOneAndUpdate(
-//     testData, /* query */
-//     {$set: testData}, /* update */
-//     { upsert: true}, function(error, data){
-//         if(error) {
-//             console.log(error);
-//         } else {
-//             console.log('save');
-//         }
-//     });
 
-//console.log(db.collection('news'));
-//var newsCollection db.
 class NewsTopicDatabase {
     
     save(parsedNewsList = []){
@@ -99,9 +70,7 @@ class NewsTopicDatabase {
                 }
             );
         }
-        // Logger.info(`Total ${savedCount} item(s) saved`);
     }
-
 }
 
 module.exports.NewsTopicDatabase = NewsTopicDatabase;
