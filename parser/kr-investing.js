bb13e1d845a574d26c390e8be43c743379c4bb42
an <adh0463@gmail.com>
2022-06-22 21:46:47 +0900

    Update: update kr-investing news channel

--- parser/kr-investing.js
+++ parser/kr-investing.js
@@ -1,7 +1,6 @@
 var krInvesting = {
     url: "https://kr.investing.com/news/cryptocurrency-news",
     postParsing: function(parsedNewsList){
-        console.log('??')
         for(var news of parsedNewsList) {
             for(const name in news){
                 if(name === 'date') {
